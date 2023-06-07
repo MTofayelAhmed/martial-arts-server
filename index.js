017d90b7488b8c796bd55316785ddab8c8ecde79
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-06-08 02:49:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require ('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000; 
+require('dotenv').config()
+
+// middle wire
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+  res.send("summer camp in running ")
+})
+
+app.listen(port, ()=> {
+  console.log(`summer camp is running on port ${port} `)
+})

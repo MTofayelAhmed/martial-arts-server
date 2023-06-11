6cb1f52483f78c80f1391be4efab7e14938d875d
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-06-11 21:21:41 +0600

    jwt token and other jwt verity and used verityJwt in carts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,6 +9,23 @@ require("dotenv").config();
 // middle wire
 app.use(cors());
 app.use(express.json());
+
+
+
+const verifyJWT = (req, res, next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: "unauthorized access"})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: "unauthorized access"})
+    }
+    req.decoded= decoded;
+    next()
+  })
+} 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhvkztn.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,62 +45,170 @@ async function run() {
 
     const usersCollection = client.db("summerdb").collection("users");
     const classCollection = client.db("summerdb").collection("classes");
-    const instructorCollection = client.db("summerdb").collection("instructors");
+    const instructorCollection = client
+      .db("summerdb")
+      .collection("instructors");
     const classCartCollection = client.db("summerdb").collection("classCart");
 
-// all users collection
-app.post('/users', async(req, res)=> {
+// JWT related issue
+app.post('/jwt', (req, res)=> {
   const user = req.body;
-  const query= {email: user.email}
-  const existingUser = await usersCollection.findOne(query)
-  console.log("existing user", existingUser)
-  if(existingUser){
-    return res.send({message: "user already exists"})
-  }
-  const result = await usersCollection.insertOne(user);
-  res.send(result)
-})
+  const token= jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '1h' })
+  res.send({token})
 
-app.get('/users', async(req, res)=> {
-  const result =  await usersCollection.find().toArray()
-  res.send(result)
+
 })
 
 
 
+    // all users collection
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log("existing user", existingUser);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+// get users to show at admin dashboard (manage users)
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
 
 
-    // first get route for popular classes . called from popular class component
+    // update users status (make admin )
+
+    app.patch('/users/admin/:id', async(req, res)=> {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: "admin", 
+        }
+      
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    // update user status( make instructors)
+
+    app.patch('/users/instructor/:id', async(req, res)=> {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: "instructor", 
+        }
+      
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+// post classes data from add a class component 
+    app.post("/classes", async (req, res) => {
+      const classInfo=  req.body;
+      const result = await classCollection.insertOne(classInfo)
+      res.send(result)
+
+    });
+    //(Home Page get for popular classes) first all classes based on  . called from popular class component
     app.get("/classes", async (req, res) => {
       const query = {};
       const options = {
-        sort: { students: -1 },
+        sort: { availableSeats: -1 },
       };
       const result = await classCollection.find(query, options).toArray();
       res.send(result);
     });
 
-    // instructors get routes to get all instructors  data in AllInsTructors component
+
+
+    // get classes based on specific instructor email
+    app.get('/instructorClasses', async(req, res)=> {
+      const email= req.query.email;
+
+    console.log(email)
+      if (!email) {
+        res.send([]);
+      }
+      const query = {email: email}
+      const result = await classCollection.find(query).toArray()
+      res.send(result)
+    })
+
+// (admin dashBoard) update class status(approved)
+app.patch('/classes/approve/:id', async(req, res)=> {
+  const id = req.params.id;
+
+  const filter = {_id: new ObjectId(id)}
+  const updateDoc = {
+    $set: {
+      status:"approved"
+    }}
+  const result = await classCollection.updateOne(filter, updateDoc)
+  res.send(result)
+
+})
+//(admin dashboard) update class status  denied
+app.patch('/classes/deny/:id', async(req, res)=> {
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)}
+  const updateDoc = {
+    $set: {
+      status: "denied", 
+    }}
+  const result = await classCollection.updateOne(filter, updateDoc)
+  res.send(result)
+
+})
+
+
+
+
+
+
+    // all instructors get  routes for AllInsTructors component(no JWT TOKEN verify)
     app.get("/instructors", async (req, res) => {
       const query = {};
       const result = await instructorCollection.find(query).toArray();
       res.send(result);
     });
 
-    // select class  cart er Route for saving(posting) selected course in classCart collection
+
+
+
+    // select class button  Route for saving(posting) selected course in classCart collection
     app.post("/carts", async (req, res) => {
       const item = req.body;
+      const id= item.classId;
+      const query= {classId: id}
+      const existingClass = await classCartCollection.findOne(query)
+      if(existingClass){
+        return res.send({message: "you have already selected the class"})
+      }
       const result = await classCartCollection.insertOne(item);
       res.send(result);
     });
+
     // get the selected course from classCart collection for individual student
-    app.get("/carts", async (req, res) => {
+    app.get("/carts", verifyJWT,   async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
-      const query = { email: email };
+const decodedEmail = req.decoded.email;
+if(email !== decodedEmail){
+  return res.status(401).send({error: true, message: "unauthorized access"})
+}
+   const query = { email: email };
       const result = await classCartCollection.find(query).toArray();
       res.send(result);
     });
